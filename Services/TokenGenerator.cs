43c064742779201397ab31cceda0fc7b054cdc9a
Ingrid_Ramos <ingridrcs@gmail.com>
2022-12-09 22:20:15 -0300

    projeto, requisitos técnicos e funcionalidades

--- Services/TokenGenerator.cs
+++ Services/TokenGenerator.cs
@@ -1,20 +1,20 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+// using Microsoft.AspNetCore.Authentication;
+// using Microsoft.AspNetCore.Authentication.JwtBearer;
 
-namespace APITryitter.Services
-{
-  public class TokenGenerator
-  {
-    public string Generate()
-    {
-      var tokenHandler = new JwtSecurityTokenHandler();
-      var tokenDescriptor = new SecurityTokenDescriptor()
-      {
-        SigningCredentials = new SingningCredentials()
-        {
-          new SymmectricSecurityKey(Encoding.ASCII.GetBytes(TokenConstants.Secret));
-        }
-      }
-    }
-  }
-}
+// namespace APITryitter.Services
+// {
+//   public class TokenGenerator
+//   {
+//     public string Generate()
+//     {
+//       var tokenHandler = new JwtSecurityTokenHandler();
+//       var tokenDescriptor = new SecurityTokenDescriptor()
+//       {
+//         SigningCredentials = new SingningCredentials()
+//         {
+//           new SymmectricSecurityKey(Encoding.ASCII.GetBytes(TokenConstants.Secret));
+//         }
+//       }
+//     }
+//   }
+// }
